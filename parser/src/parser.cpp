161d5c0411c1c75d04e84499d74b3f9cb876f75b
Ana Jakovljevic <ana.jakovljev98@gmail.com>
2021-04-03 00:40:00 +0200

    Doradjivanje parsera

--- parser/src/parser.cpp
+++ parser/src/parser.cpp
@@ -27,26 +27,60 @@ MassifParser::~MassifParser()
     }
 }
 
-
-
 MassifParser::ParserStatus MassifParser::parse()
 {
     using x3::lit;
     using x3::int_;
     using x3::ulong_;
 
-    using massifHeader = std::tuple<std::string, std::string, std::string>; //rHeader
-    using treeHeader = std::tuple<int, int, std::string>; //rTreeHeader
-    using node = std::tuple<int, int, std::string, std::string, std::string, int>; //rTreeNode
-    using tree = std::optional<std::tuple<treeHeader, std::vector<node>>>; //-(rTreeStructure)
-    using snapshot = std::tuple<int, ulong, int, int, int, tree>; //rSnapshot
-    // std::tuple<massifHeader, std::vector<snapshot>> result;
-    // std::tuple<int, int> result;
-    std::tuple<treeHeader, int> result;
-    bool parseStatus = x3::parse(mContent.str().begin(),
-                                 mContent.str().end(),
-                                 (massifRules::rTreeHeader) >> int_
-    , result);
+  //  using massifHeader = std::tuple<std::string, std::string, std::string>; //rHeader
+  //  using treeHeader = std::tuple<int,int,std::string>; //rTreeHeader
+  //  using node = std::tuple<int, int, std::string, std::string, std::string, int>; //rTreeNode
+  //  using tree = x3::optional<std::tuple<treeHeader, std::vector<node>>>; //-(rTreeStructure)
+  //  using snapshot = std::tuple<int, ulong, int, int, int, tree>; //rSnapshot
 
+    //using tree = std::optional<std::tuple<int,int,std::string, std::vector<std::tuple<int,int,std::string, std::string, std::string, int>>>>;
+    //using result = std::tuple<std::string, std::string, std::string,  //rHeader
+                            //std::vector<std::tuple<int,ulong,int,int,int, tree>>>; //rSnapshots
+    
+    // bez optional
+    using result = std::tuple<std::string, std::string, std::string,  //rHeader
+                             std::vector<std::tuple<int,ulong,int,int,int,
+                             int,int,std::string, std::vector<std::tuple<int,int,std::string, std::string, std::string, int>>>>>; //rSnapshots
+    
+    result res;
+
+    auto const content = std::string("desc: (none) \
+    cmd: ./a.out \
+    time_unit: i \
+    #----------- \
+    snapshot=0 \
+    #----------- \
+    time=0 \
+    mem_heap_B=0 \
+    mem_heap_extra_B=0 \
+    mem_stacks_B=0 \
+    heap_tree=empty\
+    #----------- \
+    snapshot=1 \
+    #----------- \
+    time=139703 \
+    mem_heap_B=1000 \
+    mem_heap_extra_B=16 \
+    mem_stacks_B=0 \
+    heap_tree=empty ");
+    
+    bool parseStatus = x3::parse(content.begin(),
+                                content.end(),
+                                massifRules::rHeader >> *(massifRules::rSnapshot),
+                                res);
+/*
+    std::string first;
+    std::string second;
+    std::string third;
+    std::vector<std::tuple<int,ulong,int,int,int,tree>> fourth;
+    std::tie (first,second,third,fourth) = res;   // unpacking tuple into variables
+*/
+    
     return ParserStatus::ePARSER_OK;
 }
